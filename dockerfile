7552c32b966f610ecdb395277bba23a3363ecb1b
Lucas Briese <lbriese@mail.uni-paderborn.de>
2017-09-11 17:10:14 +0200

    initial commit

---
+++ dockerfile
@@ -0,0 +1,28 @@
+FROM frolvlad/alpine-glibc:alpine-3.6
+
+EXPOSE 9987/udp 10011 30033
+
+ENV TS_DIR_NAME="teamspeak3-server"
+ENV TS_PATH="/home/${TS_DIR_NAME}" \
+	TS_GROUP_ID=10002 \
+	TS_USER_ID=10002 \
+	TS_USER=teamspeak \
+	LD_LIBRARY_PATH=".:$LD_LIBRARY_PATH"
+
+COPY ["teamspeakUpdater.sh", "/home/teamspeakUpdater.sh" ]
+	
+RUN apk add --no-cache bash=4.3.48-r1 bzip2=1.0.6-r5 coreutils=8.27-r0 curl=7.55.0-r0 grep=3.0-r0 tar=1.29-r1 && \
+	addgroup -g "${TS_GROUP_ID}" "${TS_USER}" && \
+	adduser -h "${TS_PATH}" -g "" -s "/bin/false" -G "${TS_USER}" -D -u "${TS_USER_ID}" "${TS_USER}" && \
+	chown "$TS_USER" "/home/teamspeakUpdater.sh" && \
+	chmod -R =700 "/home/teamspeakUpdater.sh" && \
+	chown "$TS_USER" "$TS_PATH" && \
+	chmod -R =700 "$TS_PATH" && \
+	apk del --quiet --no-cache --progress --purge && \
+	rm -rf /var/cache/apk/*
+
+VOLUME "$TS_PATH"
+	
+USER "$TS_USER_ID:$TS_GROUP_ID"
+	
+ENTRYPOINT "./home/teamspeakUpdater.sh"
---
+++ teamspeakUpdater.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+set -e
+
+for i in {1..5}
+do
+	echo "${i}/5 waiting for starting"
+	sleep 1s
+done
+
+#TS_DIR_NAME, TS_PATH => exported in dockerfile
+website="http://dl.4players.de/ts/releases/"
+versions=$(curl -s "$website" | grep -Po "(?<=>)(\d+(?:\.\d+)+/)(?=<)" | sort -t "." -k 1,1nr -k 2,2nr -k 3,3nr -k 4,4nr)
+downloaded=false
+server_tar="/tmp/${TS_DIR_NAME}.tar.bz2"
+dir_above_serverdir=$(echo "$TS_PATH" | grep -Po "^(.+)(?=/)")
+prefered_version="$1"
+startscript_name="ts3server_minimal_runscript.sh"
+startscript="${TS_PATH}/${startscript_name}"
+
+downloadVersion() {
+	current="${website}${1}"
+	current_file=$(curl -s "$current" | grep -Po "(?i)(?<=\")teamspeak3-server_linux_amd64[^\"]+?\.tar\.bz2" | sort -r | head --lines=1 -)
+	
+	# if a server version is found ...
+	if [ "$(echo "$current_file" | wc -m)" != "1" ]; then
+		echo "found server version: $current_file"
+		# ... download it
+		curl -s -o "${server_tar}" "${current}${current_file}"
+	fi
+}
+
+echo "looking if a version is prefered"
+# try at first prefered_version
+if [ -n "$prefered_version" ]; then
+	echo "searching for given prefered_version"
+	downloadVersion "${prefered_version}"
+	if [ -e "${server_tar}" ]; then
+		downloaded=true
+	else
+		echo "couldn't find prefered server version, switching to latest"
+	fi
+fi
+
+#if prefered version not given or failed
+if ! $downloaded ;then
+	echo "looking for latest version"
+	# find latest version and download it
+	# not every version has a server version, so we need first to find the latest version
+	for version in $versions
+	do
+		if ! $downloaded ;then
+			downloadVersion "${version}"
+			if [ -e "${server_tar}" ]; then
+				downloaded=true
+			fi
+		fi
+	done
+fi
+
+echo "installing teamspeak version"
+files_backup=("licensekey.dat" "query_ip_blacklist.txt" "query_ip_whitelist.txt" "serverkey.dat" "ts3server.ini" "ts3server.sqlitedb")
+if $downloaded ;then
+	#remove last logs and 
+	cd "${TS_PATH}"
+	if [ -e "logs" ]; then
+		if [ -e "logs.tar" ]; then
+			if [ -e "logs.temp.tar" ]; then
+				rm "logs.temp.tar"
+			fi
+			tar --remove-files -cf "logs.temp.tar" "logs" "logs.tar"
+			mv -f "logs.temp.tar" "logs.tar"
+		else
+			tar --remove-files -cf "logs.tar" "logs"
+		fi
+	else
+		echo "directory logs doesn't exist, skipped log clear"
+	fi
+	
+	#tar downloaded to install_dir with overwrite
+	cd "/tmp/"
+	# check if there is an last workdir
+	if [ -e "${TS_DIR_NAME}" ] && [ -n "${TS_DIR_NAME}" ]; then
+		rm -rf "${TS_DIR_NAME}"
+	fi
+	
+	echo "extracting new version"
+	# exctract new version
+	tar --overwrite -xf "${server_tar}"
+	ls -A
+	# go into it
+	temp_server_dirname=`ls -F | grep / | head --lines=1`
+	cd "$temp_server_dirname"
+	
+	echo "copy needed files from installation to new version"
+	#copy own files into
+	for file in $files_backup
+	do
+		if [ -e "${TS_PATH}/${file}" ]; then
+			cp -f "${TS_PATH}/${file}" "${file}"
+		fi
+	done
+	
+	echo "removing old version of ${startscript}"
+	# remove minimal script, maybe its not in it anymore, but if we want to call it
+	if [ -e "${startscript}" ]; then
+		rm "${startscript}"
+	fi
+	
+	echo "moving new files to installation"
+	# move all files to installation #old: /tmp/${temp_server_dirname}
+	cp -rf * "${TS_PATH}"
+	
+	echo "cleanup"
+	# clear workdir
+	rm -rf "/tmp/${TS_DIR_NAME}"
+fi
+
+# start the server
+cd "$TS_PATH"
+chmod -R =700 "$TS_PATH"
+exec "./${startscript_name}"
